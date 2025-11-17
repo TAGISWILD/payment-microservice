b4714341661e116fb48b40d2a9c1501126013db6
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2025-11-17 23:27:37 +0530

    Param

--- src/main/java/in/ethiccode/paymentservice/controller/HealthController.java
+++ src/main/java/in/ethiccode/paymentservice/controller/HealthController.java
@@ -1,6 +1,8 @@
 package in.ethiccode.paymentservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,10 @@ public class HealthController {
     @GetMapping("/ping")
     public String ping() {
         System.out.println("Hello World");
-        return "payment-service: OK";
+        return "payment-service: K01";
     }
+    @GetMapping("/payment/{id}")
+public ResponseEntity<String> getPaymentById(@PathVariable("id") Long paymentId) {
+    return ResponseEntity.ok("Payment ID = " + paymentId);
+}
 }
